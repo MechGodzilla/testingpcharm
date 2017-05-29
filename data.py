e8c8b337214fecb4cc9e34f180bf570f71530d74
MechG <serpentining@gmail.com>
2017-05-29 02:40:27 -0700

    pt1

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'name':'Article One',
+            'body': 'asdfasd fasdgasdgasg asdfasdf asdf',
+            'author':'Author\'s Name',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'name': 'Article Two',
+            'body': 'asdfasd fasdgasdgasg asdfasdf asdf',
+            'author': 'Author\'s 2 Name',
+            'create_date': '04-28-2017'
+        },
+        {
+            'id': 3,
+            'name': 'Article Three',
+            'body': 'asdfasd fasdgasdgasg asdfasdf asdf',
+            'author': 'Author\'s 3 Name',
+            'create_date': '04-30-2017'
+        }
+    ]
+    return articles
